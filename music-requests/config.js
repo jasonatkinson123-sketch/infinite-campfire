85f8cae808a1ac7e8d561e806fb8999a93d01dc8
jasonatkinson123-sketch <jasonatkinson123@gmail.com>
2026-09-24 10:22:35 -0400

    Add music requests config.js

---
+++ music-requests/config.js
@@ -0,0 +1,10 @@
+window.DERBY_MUSIC_CONFIG = {
+  API_URL: "",
+  GOOGLE_FORM_URL: "https://docs.google.com/forms/d/e/1FAIpQLSd7rCHUY0MaQdLaX1Yed_JCBQKJ9gNHSyE0-a5xQqicJBCjDQ/viewform",
+  GOOGLE_FORM_FIELDS: {
+    name: "entry.92196832",
+    grade: "entry.1225702646",
+    piece: "entry.1868602513",
+    instrument: "entry.950515501"
+  }
+};
